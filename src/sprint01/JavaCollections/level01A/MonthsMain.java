ff82fbc6237c2a56a2ff73272717a88a2ad6a505
Ana Ruth <aniarinna@gmail.com>
2025-12-07 13:22:01 +0100

    feat: add Collections Exercise 1 (Month and MonthsMain)

--- src/sprint01/JavaCollections/level01A/MonthsMain.java
+++ src/sprint01/JavaCollections/level01A/MonthsMain.java
@@ -1,4 +1,73 @@
 package sprint01.JavaCollections.level01A;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
 public class MonthsMain {
+        public static void main(String[] args) {
+        List<Month> months = createdMonthList();
+
+        printWithForEach("Initial list WITHOUT August (ArrayList)", months);
+
+        insertAugust(months);
+
+        printWithForEach("List WHIT August inserted (ArrayList)", months);
+
+        months.add(new Month("March"));
+        months.add(new Month("March"));
+
+        printWithForEach("List with duplicates added (ArrayList)", months);
+
+        Set<Month> monthSet = new LinkedHashSet<>(months);
+
+        printWithForEach("Months stored inside a LinkedHashSet (No duplicates)", monthSet);
+
+        printWithIterator("Iterating LikedHashSet using Iterador", monthSet);
+
+    }
+
+    private static List<Month> createdMonthList() {
+        List<Month> months = new ArrayList<>();
+
+        months.add(new Month("Gener"));
+        months.add(new Month("February"));
+        months.add(new Month("March"));
+        months.add(new Month("April"));
+        months.add(new Month("May"));
+        months.add(new Month("June"));
+        months.add(new Month("July"));
+        months.add(new Month("September"));
+        months.add(new Month("October"));
+        months.add(new Month("November"));
+        months.add(new Month("December"));
+
+        return months;
+    }
+
+    private static void insertAugust(List<Month> months) {
+        months.add(7, new Month("August"));
+
+    }
+
+    private static void printWithForEach(String title, Iterable<Month> collection) {
+        System.out.println("\n--- " + title + " ---");
+
+        for (Month month : collection) {
+            System.out.println(month);
+        }
+    }
+
+    private static void printWithIterator(String title, Iterable<Month> collection) {
+        System.out.println("\n--- " + title + " ---");
+
+        Iterator<Month> iterator = collection.iterator();
+
+        while (iterator.hasNext()) {
+            Month month = iterator.next();
+            System.out.println(month);
+         }
+    }
 }
